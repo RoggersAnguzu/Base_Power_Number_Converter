7f492f42143588c6a206188394a45d11aad944f3
Roggers Anguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2023-08-08 22:05:09 +0300

    Add files via upload
    Power_Base_Mathematics_converter

---
+++ POWER/POWER/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Power
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Program Calculates the Power of any Number");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("Enter the base Number: ");
+            int base_Number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Power of the Number: ");
+            int poWer = int.Parse(Console.ReadLine());
+            int result = CalculatePower(base_Number, poWer);
+
+            Console.WriteLine("The value of {0} to power {1} is {2}",base_Number,poWer,result);
+        }
+        public static int CalculatePower(int x, int y)
+        {
+            if (y == 0)
+                return 1;
+            else
+                return x * CalculatePower(x,y-1);
+        }
+    }
+}
